e4efe8300992524692f00f69678774d24816ddb1
Salads <dgentry.email@protonmail.com>
2024-06-04 06:36:27 -0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ArduinoImageConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_guiFileListView = new System.Windows.Forms.ListView();
+            this.m_guiFilesGroup = new System.Windows.Forms.GroupBox();
+            this.m_guiAddFilesButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.m_guiArrayElemSizeComboBox = new System.Windows.Forms.ComboBox();
+            this.m_guiSwapRBCheckbox = new System.Windows.Forms.CheckBox();
+            this.m_guiUseStdIntCheckbox = new System.Windows.Forms.CheckBox();
+            this.m_guiConvertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_guiFormatComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.m_guiCodeTextBox = new System.Windows.Forms.TextBox();
+            this.m_guiFilesGroup.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // m_guiFileListView
+            // 
+            this.m_guiFileListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_guiFileListView.HideSelection = false;
+            this.m_guiFileListView.Location = new System.Drawing.Point(6, 19);
+            this.m_guiFileListView.MultiSelect = false;
+            this.m_guiFileListView.Name = "m_guiFileListView";
+            this.m_guiFileListView.Size = new System.Drawing.Size(410, 250);
+            this.m_guiFileListView.TabIndex = 0;
+            this.m_guiFileListView.UseCompatibleStateImageBehavior = false;
+            this.m_guiFileListView.View = System.Windows.Forms.View.List;
+            // 
+            // m_guiFilesGroup
+            // 
+            this.m_guiFilesGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_guiFilesGroup.Controls.Add(this.m_guiFileListView);
+            this.m_guiFilesGroup.Location = new System.Drawing.Point(12, 42);
+            this.m_guiFilesGroup.Name = "m_guiFilesGroup";
+            this.m_guiFilesGroup.Size = new System.Drawing.Size(422, 276);
+            this.m_guiFilesGroup.TabIndex = 1;
+            this.m_guiFilesGroup.TabStop = false;
+            this.m_guiFilesGroup.Text = "Files";
+            this.m_guiFilesGroup.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // m_guiAddFilesButton
+            // 
+            this.m_guiAddFilesButton.Location = new System.Drawing.Point(12, 13);
+            this.m_guiAddFilesButton.Name = "m_guiAddFilesButton";
+            this.m_guiAddFilesButton.Size = new System.Drawing.Size(75, 23);
+            this.m_guiAddFilesButton.TabIndex = 1;
+            this.m_guiAddFilesButton.Text = "Select Files";
+            this.m_guiAddFilesButton.UseVisualStyleBackColor = true;
+            this.m_guiAddFilesButton.Click += new System.EventHandler(this.selectFilesButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.m_guiArrayElemSizeComboBox);
+            this.groupBox1.Controls.Add(this.m_guiSwapRBCheckbox);
+            this.groupBox1.Controls.Add(this.m_guiUseStdIntCheckbox);
+            this.groupBox1.Controls.Add(this.m_guiConvertButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.m_guiFormatComboBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 324);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 148);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Format";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Code Array Size:";
+            // 
+            // m_guiArrayElemSizeComboBox
+            // 
+            this.m_guiArrayElemSizeComboBox.FormattingEnabled = true;
+            this.m_guiArrayElemSizeComboBox.Items.AddRange(new object[] {
+            "8 bit",
+            "16 bit",
+            "32 bit"});
+            this.m_guiArrayElemSizeComboBox.Location = new System.Drawing.Point(98, 45);
+            this.m_guiArrayElemSizeComboBox.Name = "m_guiArrayElemSizeComboBox";
+            this.m_guiArrayElemSizeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.m_guiArrayElemSizeComboBox.TabIndex = 5;
+            // 
+            // m_guiSwapRBCheckbox
+            // 
+            this.m_guiSwapRBCheckbox.AutoSize = true;
+            this.m_guiSwapRBCheckbox.Location = new System.Drawing.Point(10, 95);
+            this.m_guiSwapRBCheckbox.Name = "m_guiSwapRBCheckbox";
+            this.m_guiSwapRBCheckbox.Size = new System.Drawing.Size(71, 17);
+            this.m_guiSwapRBCheckbox.TabIndex = 4;
+            this.m_guiSwapRBCheckbox.Text = "Swap RB";
+            this.m_guiSwapRBCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // m_guiUseStdIntCheckbox
+            // 
+            this.m_guiUseStdIntCheckbox.AutoSize = true;
+            this.m_guiUseStdIntCheckbox.Location = new System.Drawing.Point(10, 72);
+            this.m_guiUseStdIntCheckbox.Name = "m_guiUseStdIntCheckbox";
+            this.m_guiUseStdIntCheckbox.Size = new System.Drawing.Size(135, 17);
+            this.m_guiUseStdIntCheckbox.TabIndex = 3;
+            this.m_guiUseStdIntCheckbox.Text = "Use stdint (uint8_t, etc)";
+            this.m_guiUseStdIntCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // m_guiConvertButton
+            // 
+            this.m_guiConvertButton.Location = new System.Drawing.Point(5, 118);
+            this.m_guiConvertButton.Name = "m_guiConvertButton";
+            this.m_guiConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.m_guiConvertButton.TabIndex = 2;
+            this.m_guiConvertButton.Text = "Convert";
+            this.m_guiConvertButton.UseVisualStyleBackColor = true;
+            this.m_guiConvertButton.Click += new System.EventHandler(this.m_guiConvertButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Desired Format:";
+            // 
+            // m_guiFormatComboBox
+            // 
+            this.m_guiFormatComboBox.FormattingEnabled = true;
+            this.m_guiFormatComboBox.Items.AddRange(new object[] {
+            "1 bpp (monochrome)",
+            "16 bpp (5,6,5)",
+            "24 bpp (6,6,6)"});
+            this.m_guiFormatComboBox.Location = new System.Drawing.Point(98, 17);
+            this.m_guiFormatComboBox.Name = "m_guiFormatComboBox";
+            this.m_guiFormatComboBox.Size = new System.Drawing.Size(121, 21);
+            this.m_guiFormatComboBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.m_guiCodeTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(441, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(347, 459);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arduino Code";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(266, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Copy Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnCopyCodeButton_Click);
+            // 
+            // m_guiCodeTextBox
+            // 
+            this.m_guiCodeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_guiCodeTextBox.Location = new System.Drawing.Point(7, 18);
+            this.m_guiCodeTextBox.MaxLength = 2147483647;
+            this.m_guiCodeTextBox.Multiline = true;
+            this.m_guiCodeTextBox.Name = "m_guiCodeTextBox";
+            this.m_guiCodeTextBox.ReadOnly = true;
+            this.m_guiCodeTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.m_guiCodeTextBox.Size = new System.Drawing.Size(334, 406);
+            this.m_guiCodeTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.m_guiAddFilesButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.m_guiFilesGroup);
+            this.MinimumSize = new System.Drawing.Size(816, 416);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.m_guiFilesGroup.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView m_guiFileListView;
+        private System.Windows.Forms.GroupBox m_guiFilesGroup;
+        private System.Windows.Forms.Button m_guiAddFilesButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox m_guiFormatComboBox;
+        private System.Windows.Forms.Button m_guiConvertButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox m_guiCodeTextBox;
+        private System.Windows.Forms.CheckBox m_guiUseStdIntCheckbox;
+        private System.Windows.Forms.CheckBox m_guiSwapRBCheckbox;
+        private System.Windows.Forms.ComboBox m_guiArrayElemSizeComboBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace ArduinoImageConverter
+{
+    public partial class Form1 : Form
+    {
+        private List<string> m_filenames = new List<string>();
+        private List<string> m_safeFilenames = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            m_guiFormatComboBox.SelectedIndex = 0;
+            m_guiArrayElemSizeComboBox.SelectedIndex = 0;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectFilesButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "png images (*.png) | *.png";
+                openFileDialog.FilterIndex = 0;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    m_filenames.Clear();
+                    m_safeFilenames.Clear();
+
+                    m_filenames.AddRange(openFileDialog.FileNames);
+                    m_safeFilenames.AddRange(openFileDialog.SafeFileNames);
+
+                    foreach(string f in m_safeFilenames)
+                    {
+                        m_guiFileListView.Items.Add(f);
+                    }
+                }
+            }
+        }
+
+        private uint RangeMap(uint srcRangeStart, uint srcRangeEnd, uint destRangeStart, uint destRangeEnd, uint srcVal)
+        {
+            uint srcRange = srcRangeEnd - srcRangeStart; // Same amount of numbers, but starts from zero.
+            float srcRangePercent = srcVal / srcRange;
+
+            uint destRange = destRangeEnd - destRangeStart;
+            uint newVal = (uint)(destRangeStart + (srcRangePercent * destRange));
+            return newVal;
+        }
+
+        private string GetArrayTypeString()
+        {
+            int arrayElemSize = 8;
+            string elemType = "const unsigned char";
+            if (m_guiArrayElemSizeComboBox.SelectedIndex == 1)
+            {
+                arrayElemSize = 16;
+                elemType = "const unsigned short";
+            }
+            else if (m_guiArrayElemSizeComboBox.SelectedIndex == 2)
+            {
+                arrayElemSize = 18;
+                elemType = "const unsigned int";
+            }
+
+            if(m_guiUseStdIntCheckbox.Checked)
+            {
+                elemType = string.Format("const uint{0}_t", arrayElemSize);
+            }
+
+            return elemType;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void m_guiConvertButton_Click(object sender, EventArgs e)
+        {
+            m_guiCodeTextBox.Clear();
+            bool swapRB = m_guiSwapRBCheckbox.Checked;
+            bool useStdInt = m_guiUseStdIntCheckbox.Checked;
+            string codeOutput = string.Empty;
+
+            for (int i = 0; i < m_filenames.Count; i++)
+            {
+                string f = m_filenames[i];
+
+                List<uint> packedColors = new List<uint>();
+                Bitmap img = new Bitmap(f);
+                uint monoValByte = 0;
+                int monoValIdx = 7;
+                for (int y = 0; y < img.Height; y++)
+                {
+                    for (int x = 0; x < img.Width; x++)
+                    {
+                        Color pixel = img.GetPixel(x, y);
+                        
+                        byte srcR = pixel.R;
+                        byte srcG = pixel.G;
+                        byte srcB = pixel.B;
+                        uint srcRGB = (uint)((srcR << 16) | (srcG << 8) | (srcB << 0)); // avoid alpha channel screwing with conversions
+                        if (swapRB)
+                        {
+                            byte temp = srcR;
+                            srcR = srcB;
+                            srcB = temp;
+                        }
+
+                        if (m_guiFormatComboBox.SelectedIndex == 0) // monochrome
+                        {
+                            uint monoVal = (uint)(srcRGB > 0 ? 1 : 0);
+                            monoValByte |= monoVal << monoValIdx;
+                            monoValIdx--;
+                            if(monoValIdx == -1)
+                            {
+                                packedColors.Add(monoValByte);
+                                monoValIdx = 7;
+                                monoValByte = 0;
+                            }
+                            else if(y == img.Height - 1 && x == img.Width - 1)
+                            {
+                                packedColors.Add(monoValByte);
+                            }
+                        }
+                        else if (m_guiFormatComboBox.SelectedIndex == 1) // (5,6,5), 16 bit
+                        {
+                            uint r = RangeMap(0, 255, 0, 0b11111, srcR);
+                            uint g = RangeMap(0, 255, 0, 0b111111, srcG);
+                            uint b = RangeMap(0, 255, 0, 0b11111, srcB);
+
+                            uint val = (uint)(r << 11 | g << 5 | b);
+                            packedColors.Add(val);
+                        }
+                        else if (m_guiFormatComboBox.SelectedIndex == 2) // (6,6,6), 18 bit
+                        {
+                            uint r = RangeMap(0, 255, 0, 0b111111, srcR);
+                            uint g = RangeMap(0, 255, 0, 0b111111, srcG);
+                            uint b = RangeMap(0, 255, 0, 0b111111, srcB);
+
+                            uint val = (uint)(r << 12 | g << 6 | b);
+                            packedColors.Add(val);
+                        }
+                    }
+                } // END image processing
+
+                // Generate code for single image
+
+                // Generate comment for data array
+                codeOutput += string.Format("// {0}: {1}x{2}{3}", 
+                    m_safeFilenames[i], img.Width, img.Height, Environment.NewLine);
+
+                // Generate array
+                codeOutput += GetArrayTypeString() + " " + Path.GetFileNameWithoutExtension(m_safeFilenames[i]) + " [] PROGMEM = " + Environment.NewLine;
+                codeOutput += "{";
+
+                for (int x = 0; x < packedColors.Count; x++)
+                {
+
+                    if (x % img.Width == 0)
+                    {
+                        codeOutput += Environment.NewLine + "\t";
+                    }
+
+                    codeOutput += "0b" + Convert.ToString((int)packedColors[x], 2).PadLeft(8, '0');
+                    if (x < packedColors.Count - 1)
+                    {
+                        codeOutput += ", ";
+                    }
+                }
+
+                codeOutput += Environment.NewLine + "};" + Environment.NewLine;
+
+
+            } // END filename loop
+
+            // Generate array helper code
+            codeOutput += GetArrayTypeString() + "* allArray[] = " + Environment.NewLine;
+            codeOutput += "{" + Environment.NewLine;
+
+            for(int x = 0; x < m_safeFilenames.Count; x++)
+            {
+                codeOutput += "\t";
+                codeOutput += Path.GetFileNameWithoutExtension(m_safeFilenames[x]);
+                
+                if (x < m_safeFilenames.Count - 1)
+                {
+                    codeOutput += ",";
+                }
+
+                codeOutput += Environment.NewLine;
+            }
+
+            codeOutput += "};" + Environment.NewLine;
+
+            m_guiCodeTextBox.Text = codeOutput;
+        }
+
+        private void OnCopyCodeButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(m_guiCodeTextBox.Text);
+        }
+    }
+}
